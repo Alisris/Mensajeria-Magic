77076b6901933cc0594f11d0ceac61f48ed541b4
kris <alispromix@gmail.com>
2024-12-24 09:08:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function sendText() {
+  const textInput = document.getElementById('text-input').value;
+
+  // Verificar si el campo de texto está vacío
+  if (textInput.trim() === "") {
+    alert("Por favor, escribe algo antes de enviar.");
+    return;
+  }
+
+  // Redirigir a Proyecto 2 con el texto como parámetro en la URL
+  window.location.href = `https://mi-sitio2.com/index.html?text=${encodeURIComponent(textInput)}`;
+}
